cefbd98eba73b118ebcc20485b9675432621ff89
JuniorAlvesbr <jrnalves@gmail.com>
2019-08-23 13:30:26 -0300

    Sending the page to github

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$('.main-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 2000
+});
